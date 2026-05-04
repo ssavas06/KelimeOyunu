23686e0880d5d4c73de23590b12d71bd79122285
Selçuk Savaş <ssavas06@gmail.com>
2026-05-04 11:43:48 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const sqlite3 = require("sqlite3").verbose();
+const Database = require("better-sqlite3");
 const path = require("path");
 
 const app = express();
-const db = new sqlite3.Database("database.sqlite");
+const db = new Database("database.sqlite");
 
 app.use(express.json());
 app.use(express.static("public"));
